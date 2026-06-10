c46959c733e0dc1e220ed5ec5ad7c2afd1386570
Craig Thayer <cthayer@craigthayer.com>
2026-06-10 16:08:58 -0700

    fix bug in util/crypto/ed25519/signature

--- util/crypto/ed25519/signature.go
+++ util/crypto/ed25519/signature.go
@@ -19,7 +19,7 @@ func Sign(key ed25519.PrivateKey, data []byte) ([]byte, error) {
 	return ed25519.Sign(key, data), nil
 }
 
-// SignString expects data to be base64 encoded
+// SignString signs data with key
 func SignString(key ed25519.PrivateKey, data string) (string, error) {
 	if len(key) != ed25519.PrivateKeySize {
 		return "", errors.New("invalid private key")
@@ -29,13 +29,7 @@ func SignString(key ed25519.PrivateKey, data string) (string, error) {
 		return "", errors.New("no data provided")
 	}
 
-	d, dErr := base64.StdEncoding.DecodeString(data)
-
-	if dErr != nil {
-		return "", dErr
-	}
-
-	sigBytes, sErr := Sign(key, d)
+	sigBytes, sErr := Sign(key, []byte(data))
 
 	if sErr != nil {
 		return "", sErr
@@ -56,7 +50,7 @@ func VerifySignature(key ed25519.PublicKey, data []byte, sig []byte) bool {
 	return ed25519.Verify(key, data, sig)
 }
 
-// VerifySignatureString expects data and sig to be base64 encoded
+// VerifySignatureString expects sig to be base64 encoded
 func VerifySignatureString(key ed25519.PublicKey, data string, sig string) bool {
 	if len(key) != ed25519.PublicKeySize {
 		return false
@@ -66,17 +60,11 @@ func VerifySignatureString(key ed25519.PublicKey, data string, sig string) bool 
 		return false
 	}
 
-	d, dErr := base64.StdEncoding.DecodeString(data)
-
-	if dErr != nil {
-		return false
-	}
-
 	s, sErr := base64.StdEncoding.DecodeString(sig)
 
 	if sErr != nil {
 		return false
 	}
 
-	return VerifySignature(key, d, s)
+	return VerifySignature(key, []byte(data), s)
 }
