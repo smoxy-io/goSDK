50058828801034cc57edad3807bc36a29fa59bf0
Craig Thayer <cthayer@craigthayer.com>
2026-07-08 13:24:07 -0700

    allow more integer types in data/structures/IdAlloctor

--- util/data/structures/idAllocator.go
+++ util/data/structures/idAllocator.go
@@ -3,7 +3,7 @@ package structures
 import "sync"
 
 // IdAllocator manages a pool of IDs using a thread-safe ring buffer.
-type IdAllocator[T int | uint] struct {
+type IdAllocator[T int | uint | int64 | uint64 | int32 | uint32] struct {
 	mu        *sync.Mutex
 	capacity  T
 	buffer    []T
@@ -55,7 +55,7 @@ func (a *IdAllocator[T]) Free() T {
 }
 
 // NewIdAllocator creates an allocator with a fixed maximum size.
-func NewIdAllocator[T int | uint](capacity T) *IdAllocator[T] {
+func NewIdAllocator[T int | uint | int64 | uint64 | int32 | uint32](capacity T) *IdAllocator[T] {
 	if capacity <= 0 {
 		capacity = 1024
 	}
