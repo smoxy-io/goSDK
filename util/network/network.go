857c215768084db2d376378c5ca707f20eeb33d5
Craig Thayer <cthayer@craigthayer.com>
2026-07-17 12:32:57 -0700

    add util/network/GetOutboundIpV4() and util/network/GetOutboundIpV6()

--- util/network/network.go
+++ util/network/network.go
@@ -2,10 +2,29 @@ package network
 
 import "net"
 
-const GetOutboundIPAddr = "1.1.1.1:80"
+const (
+	GetOutboundIPAddr   = "1.1.1.1:80"
+	GetOutboundIpV6Addr = "[2606:4700:4700::1111]:80"
+)
 
 func GetOutboundIp() (net.IP, error) {
+	return GetOutboundIpV4()
+}
+
+func GetOutboundIpV4() (net.IP, error) {
 	c, err := net.Dial("udp", GetOutboundIPAddr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer c.Close()
+
+	return c.LocalAddr().(*net.UDPAddr).IP, nil
+}
+
+func GetOutboundIpV6() (net.IP, error) {
+	c, err := net.Dial("udp", GetOutboundIpV6Addr)
 
 	if err != nil {
 		return nil, err
